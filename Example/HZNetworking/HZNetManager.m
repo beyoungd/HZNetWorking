937739aedf4f07995e490b895938267ad7d9473e
huaizhan <>
2020-09-16 17:14:05 +0800

    上传库

---
+++ Example/HZNetworking/HZNetManager.m
@@ -0,0 +1,18 @@
+//
+//  HZNetManager.m
+//  HZNetworking_Example
+//
+//  Created by huaizhan on 2020/9/16.
+//  Copyright © 2020 huaizhan. All rights reserved.
+//
+
+#import "HZNetManager.h"
+
+@implementation HZNetManager
+
+- (void)testPrint:(NSString *)text {
+
+    NSLog(@"HZNetManager print == %@",text);
+
+}
+@end
---
+++ HZNetworking/Classes/HZNetManager.h
@@ -0,0 +1,19 @@
+//
+//  HZNetManager.h
+//  HZNetworking_Example
+//
+//  Created by huaizhan on 2020/9/16.
+//  Copyright © 2020 huaizhan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HZNetManager : NSObject
+
+- (void)testPrint:(NSString *)text;
+
+@end
+
+NS_ASSUME_NONNULL_END
